2236ed86b74370838ec24fc28bc39d57c4ad6712
01JAMIL <jamilbenbrahim1@gmail.com>
2022-08-23 18:26:16 +0200

    Add user forms validation

---
+++ backend/validation/user.validation.js
@@ -0,0 +1,81 @@
+const isEmpty = require('./isEmpty')
+const validator = require('validator')
+
+const validateSignUpUser = (data) => {
+
+    let errors = {}
+
+    data.firstName = !isEmpty(data.firstName) ? data.firstName : ''
+    data.lastName = !isEmpty(data.lastName) ? data.lastName : ''
+    data.email = !isEmpty(data.email) ? data.email : ''
+    data.dateOfBirth = !isEmpty(data.dateOfBirth) ? data.dateOfBirth : ''
+    data.userName = !isEmpty(data.userName) ? data.userName : ''
+    data.password = !isEmpty(data.password) ? data.password : ''
+
+    if (validator.isEmpty(data.firstName)) {
+        errors.firstNameError = 'First name is required'
+    }
+
+    if (validator.isEmpty(data.lastName)) {
+        errors.lastNameError = 'Last name is required'
+    }
+
+    if (validator.isEmpty(data.email)) {
+        errors.emailError = 'Email is required'
+    } else if (!validator.isEmail(data.email)) {
+        errors.emailError = 'Email format is not valid'
+    }
+
+    if (validator.isEmpty(data.dateOfBirth)) {
+        errors.dateOfBirthError = 'Date of birth is required'
+    }
+
+    if (validator.isEmpty(data.userName)) {
+        errors.userNameError = 'User name is required'
+    }
+
+    if (validator.isEmpty(data.password)) {
+        errors.passwordError = 'Password is required'
+    } else if (data.password.length < 8) {
+        errors.passwordError = 'Password length must be greater than 8 characters'
+    }
+
+
+    return {
+        errors,
+        valid: isEmpty(errors)
+    }
+}
+
+const validateSignInUser = (data) => {
+    let errors = {}
+
+    if (data.email) {
+        data.email = !isEmpty(data.email) ? data.email : ''
+    } else {
+        data.userName = !isEmpty(data.userName) ? data.userName : ''
+    }
+
+    data.password = !isEmpty(data.password) ? data.password : ''
+
+    const { login, password } = data
+
+    if (validator.isEmpty(login)) {
+        errors.loginError = 'User name or email is required'
+    }
+
+    if (validator.isEmpty(password)) {
+        errors.passwordError = 'Password is required'
+    }
+
+    return {
+        errors,
+        valid: isEmpty(errors)
+    }
+}
+
+
+module.exports = {
+    validateSignUpUser,
+    validateSignInUser
+}
