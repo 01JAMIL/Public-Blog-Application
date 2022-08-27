4d9a26d9cf4c97c489c32ae5ca14c5c11531d837
01JAMIL <jamilbenbrahim1@gmail.com>
2022-08-27 18:55:12 +0200

    Protect user routes

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -6,7 +6,7 @@ const { protectUserRoute } = require('../middlewares/user.middleware')
 router.get('/account', protectUserRoute, getMe)
 router.post('/signup', signup)
 router.post('/signin', signin)
-router.put('/update-profile/:username', updateProfile)
-router.put('/update-profile-pic/:username', updateProfilePic)
+router.put('/update-profile/:username', protectUserRoute, updateProfile)
+router.put('/update-profile-pic/:username', protectUserRoute, updateProfilePic)
 
 module.exports = router
