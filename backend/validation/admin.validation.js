d47e4790ab01255abb2cf941a731af2d42cd0881
01JAMIL <jamilbenbrahim1@gmail.com>
2022-09-06 20:05:58 +0200

    Update admin validation

--- backend/validation/admin.validation.js
+++ backend/validation/admin.validation.js
@@ -2,7 +2,7 @@ const isEmpty = require('./isEmpty')
 const validator = require('validator')
 
 
-const validateAdmin = (data) => {
+const validateSaveAdmin = (data) => {
     let errors = {}
 
     data.firstName = !isEmpty(data.firstName) ? data.firstName : ''
@@ -41,4 +41,31 @@ const validateAdmin = (data) => {
         errors,
         valid: isEmpty(errors),
     }
+}
+
+const validateLoginAdmin = (data) => {
+    let errors = {}
+
+    data.userName = !isEmpty(data.userName) ? data.userName : ''
+    data.password = !isEmpty(data.password) ? data.password : ''
+
+
+    if (validator.isEmpty(data.userName)) {
+        errors.userNameError = 'Username is required'
+    }
+
+    if (validator.isEmpty(data.password)) {
+        errors.passwordError = 'Password is required'
+    }
+
+
+    return {
+        errors,
+        valid: isEmpty(errors),
+    }
+}
+
+module.exports = {
+    validateSaveAdmin,
+    validateLoginAdmin
 }
