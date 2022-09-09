d1c7314a56c8d225694e3c5775d8db69a9c72be8
01JAMIL <jamilbenbrahim1@gmail.com>
2022-09-09 18:11:15 +0200

    Update comment route

--- backend/routes/comment.route.js
+++ backend/routes/comment.route.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const { getCommentById, saveComment, updateComment, deleteComment, likeComment, unlikeComment, comment } = require('../controllers/comment.controller')
 const router = express.Router()
+const { protectUserRoute } = require('../middlewares/user.middleware')
 
-router.get('/:id', getCommentById)
-router.post('/', saveComment)
-router.put('/:id', updateComment)
-router.delete('/:id', deleteComment)
-router.put('/like/:id', likeComment)
-router.put('/unlike/:id', unlikeComment)
-router.put('/comment/:id', comment)
+router.get('/:id',protectUserRoute, getCommentById)
+router.post('/',protectUserRoute, saveComment)
+router.put('/:id',protectUserRoute, updateComment)
+router.delete('/:id',protectUserRoute, deleteComment)
+router.put('/like/:id',protectUserRoute, likeComment)
+router.put('/unlike/:id',protectUserRoute, unlikeComment)
+router.put('/comment/:id',protectUserRoute, comment)
 
 module.exports = router
