ffdf5aa19899ed6b7790ce3e46b4acaaa810b733
01JAMIL <jamilbenbrahim1@gmail.com>
2022-09-14 20:14:18 +0200

    Add the store to the global app  state

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+import { Provider } from 'react-redux'
+import store from './app/store'
+import { BrowserRouter as Router } from 'react-router-dom'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>
 );
 
