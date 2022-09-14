4cd2a8a7d5eb088a68e311fdfb7ba2320f855443
01JAMIL <jamilbenbrahim1@gmail.com>
2022-09-14 17:57:08 +0200

    Update redux store

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,9 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit'
 import articleReducer from '../features/article/articleSlice'
+import authReducer from '../features/auth/userSlice'
+import commentReducer from '../features/comment/commentSlice'
+import userReducer from '../features/user/userSlice'
 
 const store = configureStore({
     reducer: {
-        article: articleReducer
+        article: articleReducer,
+        auth: authReducer,
+        comment: commentReducer,
+        user: userReducer
     }
 })
 
