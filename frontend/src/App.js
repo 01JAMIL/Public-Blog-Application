e560f39adffba25636de9560d7bd61c84dab6a71
01JAMIL <jamilbenbrahim1@gmail.com>
2022-09-16 15:38:48 +0200

    Update App.js file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,24 @@
-import './App.css';
+import './styles/form.css'
+import { useSelector } from 'react-redux'
+import SigninPage from './pages/SigninPage'
+import SignupPage from './pages/SignupPage'
+import HomePage from './pages/HomePage'
+import { Routes, Route, Navigate } from 'react-router-dom'
 
 function App() {
+
+  const { loggedIn} = useSelector(state => state.auth)
+
   return (
-    <div className="App">
-      MY BLOG APP
-    </div>
+    <>
+      <Routes>
+        <Route path='/' element={loggedIn ? <HomePage /> : <Navigate to='/signin' />} />
+        <Route path='/signup' element={!loggedIn ? <SignupPage /> : <Navigate to='/' />} />
+        <Route path='/signin' element={!loggedIn ? <SigninPage /> : <Navigate to='/' />} />
+      </Routes>
+    </>
+
   );
 }
 
-export default App;
+export default App
