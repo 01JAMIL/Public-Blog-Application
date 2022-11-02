54b638f08c17ecc8e64c8bead42ca3a99ddcd06a
01JAMIL <jamilbenbrahim1@gmail.com>
2022-11-02 22:55:03 +0100

    Add new features

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -2,11 +2,11 @@ import React, { useEffect, useRef, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
 import logo from '../assets/blog-bg.png'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome, faCaretUp, faCaretDown } from '@fortawesome/free-solid-svg-icons'
 import '../styles/navbar.css'
-
 import { getMe, logout, resetState } from '../features/auth/userSlice'
-import UserAvatar from './UserAvatar'
+import avatar from '../assets/avatar.png'
 
 const NavBar = () => {
 
@@ -20,6 +20,13 @@ const NavBar = () => {
     dispatch(logout())
     dispatch(resetState())
     navigate('/signin', { replace: true })
+  }
+
+  const style = {
+    width: '25px',
+    height: '25px',
+    border: '1px solid rgba(0, 0, 0, 0.2)',
+    borderRadius: '50%'
   }
 
   useEffect(() => {
@@ -45,9 +52,23 @@ const NavBar = () => {
         </div>
 
         <ul className='navbar-list-items'>
-          <li className='navbar-list-item'> <Link to="/" > Home </Link> </li>
-          <li onClick={() => setOpen(!open)}>
-            <UserAvatar />
+          <li className='navbar-list-item'>
+            <Link to="/" >
+              <FontAwesomeIcon icon={faHome} style={{ fontSize: '18px' }} />
+            </Link>
+            <div style={{ fontSize: '14px' }}>
+              Home
+            </div>
+          </li>
+          <li className='navbar-list-item' onClick={() => setOpen(!open)}>
+            <div style={{ height: '25px' }} >
+              <img src={user && (user.profilePic ? `../../../uploads/${user.profilePic}` : avatar)} style={style} alt='avatar' />
+            </div>
+            <div style={{ fontSize: '14px' }}>
+              You <b style={{color: 'rgba(0,0,0,0.6)'}} >{open ?
+                <FontAwesomeIcon icon={faCaretUp} />
+                : <FontAwesomeIcon icon={faCaretDown} />}</b>
+            </div>
           </li>
         </ul>
         {open && <div className='options-view' >
