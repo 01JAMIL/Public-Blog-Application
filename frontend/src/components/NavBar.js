a218f66e845e46f92d2ee9fd97c1347ec0428588
01JAMIL <jamilbenbrahim1@gmail.com>
2022-11-02 18:24:05 +0100

    Update navbar

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,17 +1,20 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
-import logo  from '../assets/blog-bg.png'
+import logo from '../assets/blog-bg.png'
 
 import '../styles/navbar.css'
 
 import { getMe, logout, resetState } from '../features/auth/userSlice'
+import UserAvatar from './UserAvatar'
 
 const NavBar = () => {
 
   const { user, token } = useSelector(state => state.auth)
   const dispatch = useDispatch()
   const navigate = useNavigate()
+  const [open, setOpen] = useState(false)
+  const listRef = useRef()
 
   const dispatchLogout = () => {
     dispatch(logout())
@@ -20,27 +23,46 @@ const NavBar = () => {
   }
 
   useEffect(() => {
+    const handleClose = (e) => {
+      if (!listRef.current.contains(e.target)) {
+        setOpen(false)
+      }
+    }
+
+    document.addEventListener('mousedown', handleClose)
     dispatch(getMe(token))
+
+    return () => {
+      document.removeEventListener('mousedown', handleClose)
+    }
   }, [dispatch, token])
 
   return (
     <div className='nav'>
-      <div className='navbar'>
+      <div className='navbar' ref={listRef}>
         <div className='navbar-brand'>
-          <img src={logo} width="100px"/> 
+          <img src={logo} width="100px" />
         </div>
 
         <ul className='navbar-list-items'>
           <li className='navbar-list-item'> <Link to="/" > Home </Link> </li>
-          <li className='navbar-list-item'>
-            <Link to="/profile" >
-              <b style={{color : '#4BB543'}}>{user && user.userName}</b>
-            </Link>
+          <li onClick={() => setOpen(!open)}>
+            <UserAvatar />
           </li>
-          <li className='navbar-list-item' > <span onClick={dispatchLogout}>Logout</span> </li>
         </ul>
+        {open && <div className='options-view' >
+          <div>
+            <Link to="/profile" >
+              <b style={{ color: '#4BB543' }}>{user && user.userName}</b>
+            </Link>
+          </div>
 
+          <div onClick={dispatchLogout}>
+            Logout
+          </div>
+        </div>}
       </div>
+
     </div>
   )
 }
