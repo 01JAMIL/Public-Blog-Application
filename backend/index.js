c4b2cc50c7c263f750501147e8133b268096b2ee
01JAMIL <jamilbenbrahim1@gmail.com>
2022-11-10 14:17:45 +0100

    Changes in index.js file

--- backend/index.js
+++ backend/index.js
@@ -46,9 +46,9 @@ app.listen(process.env.PORT || 3800, (err) => {
 
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '/frontend/build')))
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '/frontend/build', 'index.html'))
+        res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'))
     })
 }
 
