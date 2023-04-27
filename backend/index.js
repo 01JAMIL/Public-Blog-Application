841a4979c1b7aa41dc6bfbf9ae715fc5dbc4f75f
01JAMIL <jamilbenbrahim1@gmail.com>
2023-04-27 18:12:00 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -40,12 +40,12 @@ app.listen(process.env.PORT || 3800, (err) => {
 })
 
 
-if (process.env.NODE_ENV === 'production') {
+/*if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../frontend/build')))
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'))
     })
-}
+}*/
 
 
 module.exports = app
